05f14769284ab6208b151dbdc12b29ccb98c5bf7
Jerry <luanjunyi@gmail.com>
2013-06-06 19:01:07 +0800

    update

--- gossipd.go
+++ gossipd.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net"
+)
+
+var g_debug = flag.Bool("d", false, "enable debug message")
+
+func handleConnection(conn *net.Conn) {
+	log.Println("handling connection")
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	flag.Parse()
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.Println("Gossipd kicking off")
+
+	link, _ := net.Listen("tcp", ":2001")
+	
+	for {
+		conn, err := link.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConnection(&conn)
+	}
+	defer link.Close()
 }
