728536bd6288c04997cf17141b458e2c35494045
Junyi Luan <luanjunyi@douban.com>
2013-06-07 16:25:18 +0800

    add MqttFixedHeader

--- gossipd.go
+++ gossipd.go
@@ -4,12 +4,30 @@ import (
 	"flag"
 	"log"
 	"net"
+	"io"
 )
 
 var g_debug = flag.Bool("d", false, "enable debug message")
 
+type MqttFixedHeader struct {
+    MessageType uint8
+    DupFlag bool
+	Retain bool
+    QosLevel uint8
+    Length uint32
+}
+
 func handleConnection(conn *net.Conn) {
-	log.Println("handling connection")
+	log.Println("Got new conection")
+	for {
+		var buf = make([]byte, 2)
+		n, _ := io.ReadFull(*conn, buf)
+		if n != len(buf) {
+			log.Println("read header failed")
+			continue
+		}
+		log.Println("header:", string(buf))
+	}
 }
 
 func main() {
