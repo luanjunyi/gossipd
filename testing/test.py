335697b8683bebf24ac040cc1fab388a321d052f
Junyi Luan <luanjunyi@douban.com>
2013-07-05 14:46:20 +0800

    update test.py

--- testing/test.py
+++ testing/test.py
@@ -1,7 +1,10 @@
-import sys, os
+import sys, os, time
 import argparse
 import logging
 import eventlet
+from datetime import datetime
+from gossip_client import GossipClient
+from pprint import pprint
 
 eventlet.monkey_patch()
 
@@ -9,18 +12,122 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s - %(module)s(%(lineno)d)
                     level = logging.DEBUG, stream = sys.stderr)
 _logger = logging.getLogger('root')
 
-def test(worker_id, hostname, port):
-    _logger.debug("worker(%d) is created" % worker_id)
+class TestWorker(object):
+    def __init__(self, worker_id, thread_num, hostname, port):
+        self.client_id = "test_client_%d" % worker_id
+        self.worker_id = worker_id
+        self.thread_num = thread_num
+        self.hostname = hostname
+        self.port = port
+        self.client = GossipClient(client_id = self.client_id, clean_session = False)
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_publish = self.on_publish
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_message = self.on_message
+        self.client.on_unsubscribe = self.on_unsubscribe
+
+    def on_connect(self, mosq, obj, rc):
+        if rc == 0:
+            _logger.debug('worker %d connected' % self.worker_id)
+            self.status['connected'] = 1
+        else:
+            _logger.error('worker %d on_connect, rc=%d' % (self.worker_id, rc))
+
+    def on_disconnect(self, mosq, obj, rc):
+        if rc == 0:
+            _logger.debug('worker %d disconnected normally' % self.worker_id)
+            self.status['disconnected'] = 1
+        else:
+            _logger.error('worker %d lost connection to server' % self.worker_id)
+
+    def on_publish(self, mosq, obj, mid):
+        _logger.debug('on worker %d, published mid=%d' % (self.worker_id, mid))
+        self.status['published'].add(mid)
+
+    def on_subscribe(self, mosq, obj, mid, qos_list):
+        _logger.debug('on worker %d, subscribed mid=%d' % (self.worker_id, mid))
+        self.status['subscribed'].add(mid)
+
+    def on_unsubscribe(self, mosq, obj, mid):
+        _logger.debug('on woker %d, unsubscribed mid=%d' % (self.worker_id, mid))
+        self.status['unsubscribed'].add(mid)
+
+    def on_message(self, mosq, obj, msg):
+        self.status['message'][int(msg.topic)].add(int(msg.payload))
+        _logger.debug('worker %d got message, topic=(%s), payload=(%s)' % 
+                      (self.worker_id, msg.topic, msg.payload))
+        
+
+    def start(self):
+        self.status = self._build_status()
+        self.client.connect(self.hostname, self.port)
+
+        for i in xrange(1, self.thread_num+1):
+            topic = str(i)
+            self.client.subscribe(topic)
+
+        _logger.debug("worker %d waiting for subscribe done" % self.worker_id)
+        time.sleep(5)
+
+        for i in xrange(1, self.thread_num+1):
+            topic = str(i)
+            payload = str(self.worker_id)
+            self.client.publish(topic, payload)
+
+        _logger.debug("worker %d waiting for publish done" % self.worker_id)
+        time.sleep(5)
+
+
+        for i in xrange(1, self.thread_num+1):
+            topic = str(i)
+            self.client.unsubscribe(topic)
+
+
+        start_time = datetime.now()
+        while True:
+            ret = self.client.loop()
+            now = datetime.now()
+            if (now - start_time).seconds > 10:
+                self.client.disconnect()
+                break
+
+
+    def _build_status(self):
+        return {'connected': 0,
+                'disconnected': 0,
+                'published': set(),
+                'subscribed': set(),
+                'message': dict(zip(range(1, self.thread_num + 1), [set()] * self.thread_num)),
+                'unsubscribed': set(),
+                }
+
+    def collect(self):
+        return self.status
+
+
+def test(worker_id, thread_num, hostname, port):
+    worker = TestWorker(worker_id, thread_num, hostname, port)
+    _logger.debug("worker(%d) is started" % worker_id)
+    worker.start()
+    return worker.collect()
+    _logger.debug("worker(%d) is done" % worker_id)
+
+    
 
 def start_testing(hostname, port, thread_num, sleep):
     _logger.info("Testing, %s:%d, thread:%d, sleep:%d" % (hostname, port, thread_num, sleep))
     workers = list()
     for i in xrange(thread_num):
-        worker = eventlet.spawn(test, i, hostname, port)
+        worker = eventlet.spawn(test, i+1, thread_num, hostname, port)
         workers.append(worker)
 
+    statuses = list()
     for worker in workers:
-        worker.wait()
+        statuses.append(worker.wait())
+
+    for status in statuses:
+        pprint(status)
 
 
 def main():
